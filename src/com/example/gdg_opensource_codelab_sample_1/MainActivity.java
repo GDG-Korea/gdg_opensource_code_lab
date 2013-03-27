71fe4366751fb2e4c0474a125239be0c581f283e
Alan Jeon <skyisle@gmail.com>
2013-03-27 21:46:18 +0900

    YouTubeChannelClient 추가, Maven pom 파일 추가

--- src/com/example/gdg_opensource_codelab_sample_1/MainActivity.java
+++ src/com/example/gdg_opensource_codelab_sample_1/MainActivity.java
@@ -1,33 +1,69 @@
 
 package com.example.gdg_opensource_codelab_sample_1;
 
-import android.os.Bundle;
+import com.google.api.services.youtube.model.Playlist;
+import com.google.api.services.youtube.model.PlaylistItem;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 import net.simonvt.menudrawer.MenuDrawer;
 
-public class MainActivity extends SherlockFragmentActivity {
+import android.os.Bundle;
+import android.util.Log;
 
+import java.util.List;
+
+public class MainActivity extends SherlockFragmentActivity
+        implements YouTubeChannelClient.Callbacks {
+    public static final String YOUTUBE_API_KEY = "AIzaSyCwvRaeu1TqEG0ONmCcssVNeSTeHJx-eqY";
+    public static final String ANDROID_DEVELOPER_CHANNEL_ID = "UCVHFbqXqoYvEWM1Ddxl0QDg";
+    private static final String TAG = "MainActivity";
     private MenuDrawer mDrawer;
+    private YouTubeChannelClient mYouTubeClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle(R.string.hello_opensoruce_code_lab);
-        
+
         mDrawer = MenuDrawer.attach(this);
         mDrawer.setContentView(R.layout.activity_main);
-        mDrawer.setMenuView(R.layout.activity_menu);        
+        mDrawer.setMenuView(R.layout.activity_menu);
+
+        mYouTubeClient = YouTubeChannelClient
+                .getYouTubeChannelClient(YOUTUBE_API_KEY, ANDROID_DEVELOPER_CHANNEL_ID, this);
+        mYouTubeClient.getPlayList();
+    }
+
+    @Override
+    public void onLoadPlaylist(List<Playlist> playlist) {
+        for (Playlist item : playlist) {
+            Log.d(TAG, "channedId = " + item.getId());
+        }
+
+        if (playlist.size() > 0) {
+            mYouTubeClient.getPlaylistItem(playlist.get(playlist.size() - 1).getId());
+        }
+    }
+
+    @Override
+    public void onLoadPlaylistItem(List<PlaylistItem> playlistItem) {
+        for (PlaylistItem item : playlistItem) {
+            Log.d(TAG, "title = " + item.getSnippet().getTitle());
+            Log.d(TAG, "desc = " + item.getSnippet().getDescription());
+            Log.d(TAG, "thumbnails = " + item.getSnippet().getThumbnails().getDefault().getUrl());
+
+            Log.d(TAG, "videoId = " + item.getContentDetails().getVideoId());
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.abs__home:
                 //fall through
             case android.R.id.home:
@@ -37,5 +73,6 @@ public class MainActivity extends SherlockFragmentActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 
 }//end of class
---
+++ src/com/example/gdg_opensource_codelab_sample_1/YouTubeChannelClient.java
@@ -0,0 +1,114 @@
+package com.example.gdg_opensource_codelab_sample_1;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Playlist;
+import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
+import com.google.api.services.youtube.model.PlaylistListResponse;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.List;
+
+public class YouTubeChannelClient {
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = new GsonFactory();
+    private final String mApiKey;
+    private final String mChannelID;
+    private final YouTube mYouTube;
+    Callbacks mChannelCallbacks;
+
+    private YouTubeChannelClient(String apiKey, String channelID, Callbacks callbacks) {
+        mApiKey = apiKey;
+        mChannelID = channelID;
+        mChannelCallbacks = callbacks;
+
+        mYouTube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+                new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                })
+                .setApplicationName("youtube-channel-list-client")
+                .build();
+    }
+
+    public static YouTubeChannelClient getYouTubeChannelClient(String apiKey, String channelID,
+            Callbacks callbacks) {
+        return new YouTubeChannelClient(apiKey, channelID, callbacks);
+    }
+
+    public void getPlayList() {
+        new AsyncTask<Void, Void, List<Playlist>>() {
+
+            @Override
+            protected List<Playlist> doInBackground(Void... params) {
+                PlaylistListResponse searchListResponse = null;
+                try {
+                    YouTube.Playlists.List request = mYouTube.playlists().list("snippet");
+                    request.setKey(mApiKey);
+                    request.setChannelId(mChannelID);
+                    request.setMaxResults(50L);
+                    searchListResponse = request.execute();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return searchListResponse == null ? null : searchListResponse.getItems();
+            }
+
+            @Override
+            protected void onPostExecute(List<Playlist> playlists) {
+                mChannelCallbacks.onLoadPlaylist(playlists);
+            }
+
+        }.execute();
+
+    }
+
+    public void getPlaylistItem(final String playListId) {
+        new AsyncTask<Void, Void, List<PlaylistItem>>() {
+
+            @Override
+            protected List<PlaylistItem> doInBackground(Void... params) {
+                PlaylistItemListResponse playlistItemListResponse = null;
+                try {
+                    YouTube.PlaylistItems.List request = mYouTube.playlistItems()
+                            .list("snippet, contentDetails");
+                    request.setPlaylistId(playListId);
+                    request.setKey(mApiKey);
+                    request.setMaxResults(50L);
+
+                    playlistItemListResponse = request.execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return playlistItemListResponse == null ? null
+                        : playlistItemListResponse.getItems();
+            }
+
+            @Override
+            protected void onPostExecute(List<PlaylistItem> playlist) {
+                mChannelCallbacks.onLoadPlaylistItem(playlist);
+            }
+
+        }.execute();
+    }
+
+    public interface Callbacks {
+        public void onLoadPlaylist(List<Playlist> playlist);
+        public void onLoadPlaylistItem(List<PlaylistItem> playlistItem);
+    }
+}
